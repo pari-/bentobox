39b211c05f206d7413be16f7e9a044fc9e332120
Patrick Ringl <patrick_@freenet.de>
2017-09-02 07:59:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# ![bento box](https://api.emojione.com/emoji/1f371/download/32) bentobox
+
+A convenience layer for building bento-based Vagrant boxes
+
+<!-- toc -->
+
+- [Usage](#usage)
+  * [local ISO](#local-iso)
+  * [remote ISO](#remote-iso)
+- [Configuration](#configuration)
+- [Dependencies](#dependencies)
+- [License](#license)
+- [Author Information](#author-information)
+
+<!-- tocstop -->
+
+## Usage
+
+Basically you have two options:
+
+### local ISO
+
+Put the appropriate ISO in `${ISO_DIR}` and run:
+
+```bash
+BENTO_BOX_VERSION="9.1" bash build.sh
+```
+
+(this is useful for i.e. offline builds and/or not relying on packer's cache)
+
+### remote ISO
+
+Or just run the following, which will download the appropriate ISO automatically:
+
+```bash
+PACKER_OPTIONS="-only=parallels-iso" BENTO_BOX_VERSION="9.1" bash build.sh
+```
+
+## Configuration
+
+The following configuration options exist and can be changed by injecting them
+via the shell:
+
+option | default
+------ | -------
+`BENTO_BOX_ARCH` | `amd64` 
+`BENTO_BOX_NAME` | `parallels/${BENTO_BOX_TYPE}-${BENTO_BOX_VERSION}`
+`BENTO_BOX_TYPE` | `debian` 
+`BENTO_BOX_VERSION` | `9.1` 
+`BENTO_DIR` | `${HOME}/development/bento` 
+`BENTO_REPO` | `https://github.com/pari-/bento` 
+`BENTO_REPO_BRANCH` | `local_mods`
+`ISO_DIR` | `${HOME}/isos` 
+`PACKER_DIR` | `${HOME}/Downloads/packer` 
+`PACKER_DOWNLOAD_URL` | `https://releases.hashicorp.com/packer/${PACKER_VERSION}/${PACKER_DOWNLOAD_FILE}` 
+`PACKER_DOWNLOAD_FILE` | `packer_${PACKER_VERSION}_darwin_${BENTO_BOX_ARCH}.zip` 
+`PACKER_VERSION` | `1.0.4` 
+`PACKER_OPTIONS` | `-var mirror=${ISO_DIR}/ -var mirror_directory= -only=parallels-iso`
+`VAGRANT_BOX_DIR` | `${HOME}/config/vagrant_boxes` 
+
+## License
+
+MIT
+
+## Author Information
+
+* Patrick Ringl
+* Emoji artwork provided by [EmojiOne](https://www.emojione.com)
---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+BENTO_BOX_ARCH=${BENTO_BOX_ARCH:-"amd64"}
+BENTO_BOX_TYPE=${BENTO_BOX_TYPE:-"debian"}
+BENTO_BOX_VERSION=${BENTO_BOX_VERSION:-"9.1"}
+BENTO_BOX_NAME=${BENTO_BOX_NAME:-"parallels/${BENTO_BOX_TYPE}-${BENTO_BOX_VERSION}"}
+BENTO_DIR=${BENTO_DIR:-"${HOME}/development/bento"}
+BENTO_REPO=${BENTO_REPO:-"https://github.com/pari-/bento"}
+BENTO_REPO_BRANCH=${BENTO_REPO_BRANCH:-"local_mods"}
+ISO_DIR=${ISO_DIR:-"${HOME}/isos"}
+PACKER_DIR=${PACKER_DIR:-"${HOME}/Downloads/packer"}
+PACKER_VERSION=${PACKER_VERSION:-"1.0.4"}
+PACKER_DOWNLOAD_FILE=${PACKER_DOWNLOAD_FILE:-"packer_${PACKER_VERSION}_darwin_${BENTO_BOX_ARCH}.zip"}
+PACKER_DOWNLOAD_URL=${PACKER_DOWNLOAD_URL:-"https://releases.hashicorp.com/packer/${PACKER_VERSION}"}
+PACKER_OPTIONS=${PACKER_OPTIONS:-"-var mirror=${ISO_DIR}/ -var mirror_directory= -only=parallels-iso"}
+VAGRANT_BOX_DIR=${VAGRANT_BOX_DIR:-"${HOME}/config/vagrant_boxes"}
+
+#
+# create PACKER_DIR
+#
+mkdir -p "${PACKER_DIR}/${PACKER_VERSION}"
+
+#
+# retrieve, unpack and cleanup PACKER_VERSION
+#
+if [ ! -f "${PACKER_DIR}/${PACKER_VERSION}/packer" ];
+then
+	wget -O "${PACKER_DIR}/${PACKER_VERSION}/${PACKER_DOWNLOAD_FILE}" "${PACKER_DOWNLOAD_URL}/${PACKER_DOWNLOAD_FILE}"
+	unzip "${PACKER_DIR}/${PACKER_VERSION}/${PACKER_DOWNLOAD_FILE}" -d "${PACKER_DIR}/${PACKER_VERSION}/"
+	rm "${PACKER_DIR}/${PACKER_VERSION}/${PACKER_DOWNLOAD_FILE}"
+fi
+
+#
+# setup my bento repo
+#
+if [ ! -d "${BENTO_DIR}" ];
+then
+	git clone "${BENTO_REPO}" "${BENTO_DIR}"
+	cd "${BENTO_DIR}" && git checkout "${BENTO_REPO_BRANCH}"
+fi
+
+#
+# build the debian vagrant box 
+#
+cd "${BENTO_DIR}/${BENTO_BOX_TYPE}" && "${PACKER_DIR}/${PACKER_VERSION}/packer" build ${PACKER_OPTIONS} "${BENTO_BOX_TYPE}-${BENTO_BOX_VERSION}-${BENTO_BOX_ARCH}.json"
+
+#
+# move the built box to VAGRANT_BOX_DIR
+#
+mv "${BENTO_DIR}/builds/${BENTO_BOX_TYPE}-${BENTO_BOX_VERSION}.parallels.box" "${VAGRANT_BOX_DIR}"
+
+#
+# add the built vagrant box
+#
+vagrant box add "${VAGRANT_BOX_DIR}/${BENTO_BOX_TYPE}-${BENTO_BOX_VERSION}.parallels.box" --name "${BENTO_BOX_NAME}"
